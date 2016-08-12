36271b7d07305648d54944aafe272bdded47f6ad
Tiago Ferreira <tiagoffcc@hotmail.com>
2016-08-12 16:54:34 +0100

    refact(TEL-789): some refactor on buildmetricslist function for blacklist type.

--- src/metrics-list.js
+++ src/metrics-list.js
@@ -53,6 +53,7 @@ class MetricsList {
                 let availableAWSRegions = Util.getAWSAvailableRegions();
                 let blacklistedRegions = Object.keys(this[_config].statfulAwsCollector.metricsList);
                 let auxMetricsPerRegion = {};
+                let requestsPromises = [];
 
                 series([
                     (taskCallback) => {
@@ -69,106 +70,84 @@ class MetricsList {
                     (taskCallback) => {
                         // Get available metrics and filter it by blacklist
                         eachOf(auxMetricsPerRegion, (metrics, region, eachOfCallback) => {
-                            let cloudWatch = new AWS.CloudWatch({
-                                accessKeyId: this[_config].statfulAwsCollector.credentials.accessKeyId,
-                                secretAccessKey: this[_config].statfulAwsCollector.credentials.secretAccessKey,
-                                region: region
+                            each(metrics, (metric, eachCallback) => {
+                                requestsPromises.push(this[_cloudWatchListMetrics](region, metric));
+                                eachCallback(null);
+                            }, (err) => {
+                                eachOfCallback(null);
                             });
-                            let hasNextToken = null;
-
-                            doWhilst(
-                                (doDuringCallback)=> {
-                                    let reqParams = {};
-
-                                    if (hasNextToken) {
-                                        reqParams.NextToken = hasNextToken;
-                                    }
-
-                                    cloudWatch.listMetrics(reqParams, (err, data) => {
-                                        if (err === null) {
-                                            hasNextToken = data.NextToken;
-
-                                            if (data.Metrics && data.Metrics.length > 0) {
-                                                let metricsToFilterByBlacklist = data.Metrics;
-
-                                                filter(metricsToFilterByBlacklist, (metric, filterCallback) => {
-                                                    // Test if metric is to filter
-                                                    let includeMetricInResults = true;
-                                                    let blacklistedMetricsForCurrentRegion = this[_config].statfulAwsCollector.metricsList[region];
-
-                                                    if (blacklistedMetricsForCurrentRegion && blacklistedMetricsForCurrentRegion.length > 0) {
-                                                        each(blacklistedMetricsForCurrentRegion, (blackListedMetric, eachCallback) => {
-                                                            // Test for name and namespace
-                                                            if ((metric.MetricName && blackListedMetric.MetricName && metric.MetricName === blackListedMetric.MetricName) ||
-                                                                (metric.Namespace && blackListedMetric.Namespace && metric.Namespace === blackListedMetric.Namespace)) {
-                                                                includeMetricInResults = false;
-                                                            }
-
-                                                            if (includeMetricInResults) {
-                                                                // Test by dimensions and remove blacklisted dimensions
-                                                                let dimensionsToRemove = [];
-                                                                for (let i = 0; i < metric.Dimensions; i++) {
-                                                                    for (let j = 0; j < blackListedMetric.Dimensions; j++) {
-                                                                        let metricDimension = metric.Dimensions[i];
-                                                                        let blackListedMetricDimension = blackListedMetric.Dimensions[j];
-
-                                                                        if (blackListedMetricDimension.Name && blackListedMetricDimension.Value) {
-                                                                            if (metricDimension.Name === blackListedMetricDimension.Name &&
-                                                                                metricDimension.Value === blackListedMetricDimension.Value) {
-                                                                                // Remove metric.Dimensions[i]
-                                                                                dimensionsToRemove.push(i);
-                                                                            }
-                                                                        } else {
-                                                                            if (metricDimension.Name && blackListedMetricDimension.Name && metricDimension.Name === blackListedMetricDimension.Name) {
-                                                                                // Remove metric.Dimensions[i]
-                                                                                dimensionsToRemove.push(i);
-                                                                            }
-                                                                        }
-                                                                    }
-                                                                }
-
-                                                                // Remove found blacklisted dimensions
-                                                                for (let i = dimensionsToRemove.length - 1; i >= 0; i--) {
-                                                                    metric.Dimensions.splice(dimensionsToRemove[i], 1);
-                                                                }
-                                                            }
-
-                                                            eachCallback(null);
-                                                        }, (err) => {
-                                                            filterCallback(null, includeMetricInResults);
-                                                        });
-                                                    } else {
-                                                        filterCallback(null, includeMetricInResults);
-                                                    }
-                                                }, (err, filteredMetrics) => {
-                                                    if (filteredMetrics && filteredMetrics.length > 0) {
-                                                        auxMetricsPerRegion[region] = auxMetricsPerRegion[region].concat(filteredMetrics);
-                                                    }
-                                                    doDuringCallback(null);
-                                                });
-                                            } else {
-                                                doDuringCallback(null);
-                                            }
-                                        } else {
-                                            doDuringCallback(null);
-                                        }
-                                    });
-                                },
-                                () => {
-                                    return hasNextToken;
-                                },
-                                (err) => {
-                                    eachOfCallback(null);
-                                });
-
                         }, (err) => {
                             taskCallback(null);
                         });
                     }
                 ],
                 (err, results) => {
-                    this[_metricsPerRegion] = auxMetricsPerRegion;
-                    resolve();
+                    Promise.all(requestsPromises).then( (allRequestsData) => {
+                        each(allRequestsData, (requestData, eachCallback) => {
+                            if (requestData.data.length > 0) {
+                                let metricsToFilterByBlacklist = requestData.data;
+
+                                filter(metricsToFilterByBlacklist, (metric, filterCallback) => {
+                                    // Test if metric is to filter
+                                    let includeMetricInResults = true;
+                                    let blacklistedMetricsForCurrentRegion = this[_config].statfulAwsCollector.metricsList[region];
+
+                                    if (blacklistedMetricsForCurrentRegion && blacklistedMetricsForCurrentRegion.length > 0) {
+                                        each(blacklistedMetricsForCurrentRegion, (blackListedMetric, eachCallback) => {
+                                            // Test for name and namespace
+                                            if ((metric.MetricName && blackListedMetric.MetricName && metric.MetricName === blackListedMetric.MetricName) ||
+                                                (metric.Namespace && blackListedMetric.Namespace && metric.Namespace === blackListedMetric.Namespace)) {
+                                                includeMetricInResults = false;
+                                            }
+
+                                            if (includeMetricInResults) {
+                                                // Test by dimensions and remove blacklisted dimensions
+                                                let dimensionsToRemove = [];
+                                                for (let i = 0; i < metric.Dimensions; i++) {
+                                                    for (let j = 0; j < blackListedMetric.Dimensions; j++) {
+                                                        let metricDimension = metric.Dimensions[i];
+                                                        let blackListedMetricDimension = blackListedMetric.Dimensions[j];
+
+                                                        if (blackListedMetricDimension.Name && blackListedMetricDimension.Value) {
+                                                            if (metricDimension.Name === blackListedMetricDimension.Name &&
+                                                                metricDimension.Value === blackListedMetricDimension.Value) {
+                                                                // Remove metric.Dimensions[i]
+                                                                dimensionsToRemove.push(i);
+                                                            }
+                                                        } else {
+                                                            if (metricDimension.Name && blackListedMetricDimension.Name && metricDimension.Name === blackListedMetricDimension.Name) {
+                                                                // Remove metric.Dimensions[i]
+                                                                dimensionsToRemove.push(i);
+                                                            }
+                                                        }
+                                                    }
+                                                }
+
+                                                // Remove found blacklisted dimensions
+                                                for (let i = dimensionsToRemove.length - 1; i >= 0; i--) {
+                                                    metric.Dimensions.splice(dimensionsToRemove[i], 1);
+                                                }
+                                            }
+
+                                            eachCallback(null);
+                                        }, (err) => {
+                                            filterCallback(null, includeMetricInResults);
+                                        });
+                                    } else {
+                                        filterCallback(null, includeMetricInResults);
+                                    }
+                                }, (err, filteredMetrics) => {
+                                    if (filteredMetrics && filteredMetrics.length > 0) {
+                                        auxMetricsPerRegion[region] = auxMetricsPerRegion[region].concat(filteredMetrics);
+                                    }
+                                });
+                            }
+                            eachCallback(null);
+                        }, (err) => {
+                            this[_metricsPerRegion] =  auxMetricsPerRegion;
+                            resolve();
+                        });
+                    });
                 });
             }
         });
