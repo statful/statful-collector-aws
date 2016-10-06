7f037a8889b688f907fe28e66724f88b0c5e46b1
Tiago Ferreira <tiagoffcc@hotmail.com>
2016-10-06 18:17:19 +0100

    feat(NA): added a started managed command to the collector cli.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var Config = require('./lib/config.js').default;
 var StatfulCollectorAws = require('./lib/collector.js').default;
 var fs = require('fs-extra');
 var path = require('path');
+var pm2 = require('pm2');
 
 var generateConfig = function(configPath) {
     var normalizedPath = path.normalize(configPath);
@@ -43,16 +44,43 @@ var start = function(configPath) {
     });
 };
 
+var startManaged = function(configPath) {
+    return new Promise(function(resolve, reject) {
+        pm2.connect(function(err) {
+            if (err) {
+                reject(err);
+            } else {
+                pm2.start({
+                    script: 'statful-collector-aws',
+                    args: 'start ' + configPath
+                }, function(err) {
+                    pm2.disconnect();
+
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+            }
+        });
+
+
+    });
+};
+
 var cli = function() {
     var yargs = require('yargs')
         .usage('Usage: $0 [command] <path>')
         .command('generate-config <path>', 'Generate a default config for Statful Collector AWS on given path.')
         .command('start <path>', 'Start the Statful Collector AWS with a config on the given path.')
+        .command('start-managed <path>', 'Start the Statful Collector AWS, managed by pm2, with a config on the given path.')
         .demand(1)
         .strict()
         .example('$0 generate-config /etc/statful-collector-aws/conf', 'Generates a default Statful Collector AWS ' +
             'config file on /etc/statful-collector-aws/conf/ with name statful-collector-aws-conf.json')
-        .example('$0 start /etc/statful-collector-aws/conf/statful-collector-aws-conf.json', 'Starts the Statful Collector AWS with given config.')
+        .example('$0 start /etc/statful-collector-aws/conf/statful-collector-aws-conf.json', 'Starts the Statful Collector AWS with the given config.')
+        .example('$0 start-managed /etc/statful-collector-aws/conf/statful-collector-aws-conf.json', 'Starts the Statful Collector AWS, managed by pm2, with the given config.')
         .help('help')
         .alias('h', 'help')
         .epilog('Copyright 2016 Statful.');
@@ -64,6 +92,15 @@ var cli = function() {
             start(path).then(
                 function(returnedPath) {
                     return console.log('Statful Collector AWS successfully loaded with configuration file at \'' + returnedPath + '\'');
+                },
+                function(error) {
+                    return console.error(error);
+                }
+            );
+        } else if (argv._[0] === "start-managed") {
+            startManaged(path).then(
+                function() {
+                    return console.log('Pm2 successfully request a spawn for Statful Collector AWS process.');
                 },
                 function(error) {
                     return console.error(error);
@@ -84,4 +121,5 @@ var cli = function() {
 
 exports.generateConfig = generateConfig;
 exports.start = start;
+exports.startManaged = startManaged;
 exports.cli = cli;
